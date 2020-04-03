2b4a2200db143b6c26b0155e3e9d205642a624ee
mechevarria <mike.echevarria@gmail.com>
2020-04-03 13:20:35 -0400

    cleanup and refactoring

--- cf-angular.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# list domains, print first column, print 3rd line
-domain=$(cf domains | awk '{print $1}' | sed -n 3p)
-keycloak_app=keycloak-coreui
-keycloak_url=https://$keycloak_app.$domain/auth
-
-echo $keycloak_url
