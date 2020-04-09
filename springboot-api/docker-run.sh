520657ac04b929a21ce1e1390bdb2d8687a66464
mechevarria <mike.echevarria@gmail.com>
2020-04-09 12:55:09 -0400

    add docker net for container to container networking

--- springboot-api/docker-run.sh
+++ springboot-api/docker-run.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+# create network for other containers to communicate via name
+docker network create app-net
+
+docker rm springboot-api
+
 docker run \
+    --name=springboot-api \
+    --network app-net \
     -p 8080:8080 \
     --env KEYCLOAK=$KEYCLOAK \
     quay.io/mechevarria/springboot-api
