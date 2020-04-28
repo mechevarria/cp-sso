e2d77c3bd3e5637059e055acccdba00cac1ba54b
Mike Echevarria <mike.echevarria@gmail.com>
2020-04-28 10:53:46 -0400

    working script for automatic deployment

--- cf-db.sh
+++ cf-db.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 
-# multiapps=$(cf plugins | grep multiapps)
-# if [[ -z "$multiapps" ]]; then
-#   echo "multiapps plugin is required: 'cf install-plugin multiapps'"
-#   exit 1
-# fi
+multiapps=$(cf plugins | grep multiapps)
+if [[ -z "$multiapps" ]]; then
+  echo "multiapps plugin is required: 'cf install-plugin multiapps'"
+  exit 1
+fi
 
-# mbt=$(npm -g ls --depth=0 | grep mbt)
-# if [[ -z "$mbt" ]]; then
-#   echo "cloud-mta-build-tool is required: 'npm install -g mbt'"
-#   exit 1
-# fi
+cf deploy ./ -f
 
-# mbt build -p cf -t .
-
-#mtar=$(ls cp-hana*.mtar)
-#cf deploy $mtar -f
+cf create-service-key hdi-hana hdi-hana-key
