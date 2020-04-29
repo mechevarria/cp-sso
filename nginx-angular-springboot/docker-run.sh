2ea8a99a7373ab95d527e643bf0ca4074981df2c
Mike Echevarria <mike.echevarria@gmail.com>
2020-04-29 14:09:43 -0400

    add springboot integration and docker network

--- nginx-angular-springboot/docker-run.sh
+++ nginx-angular-springboot/docker-run.sh
@@ -2,6 +2,8 @@
 
 docker run \
     -p 80:80 \
+    --network app-net \
     --env MAPBOX_TOKEN=$MAPBOX_TOKEN \
     --env KEYCLOAK=$KEYCLOAK \
+    --env SPRINGBOOT_URL=http://springboot-api:8080/ \
     quay.io/mechevarria/nginx-angular-springboot
