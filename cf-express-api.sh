f5527c98f5073f4bc97c925298067dd6cbbe734d
Mike Echevarria <mike.echevarria@gmail.com>
2020-05-01 12:33:29 -0400

    script to deploy node.js app on cloud platform

---
+++ cf-express-api.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+status=$(cf app keycloak-coreui | sed -n 3p)
+if [[ $status = "FAILED" ]]; then
+  exit 1
+else
+  keycloak_url=https://$(cf app keycloak-coreui | awk '{print $2}' | sed -n 5p)/auth
+  echo keycloak_url=$keycloak_url
+fi
+
+service=hdi_hana
+status=$(cf service $service | sed -n 3p)
+if [[ $status = "FAILED" ]]; then
+  exit 1
+fi
+
+app=express-api
+
+cf push $app \
+    -m 64M \
+    -k 2048M \
+    --docker-image quay.io/mechevarria/$app \
+    --no-start
+
+cf se $app KEYCLOAK_URL $keycloak_url
+cf se $app KEYCLOAK false
+
+# bind hana service
+cf bind-service $app $service
+
+cf start $app
