ec67abd7f30fc4b4c4b516b65856cbfa60ee12ad
Mike Echevarria <mike.echevarria@gmail.com>
2020-05-08 13:08:59 -0400

    add express-api url parsing and variable setting

--- cf-nginx-angular.sh
+++ cf-nginx-angular.sh
@@ -16,6 +16,14 @@ else
   echo springboot_url=$springboot_url
 fi
 
+status=$(cf app express-api | sed -n 3p)
+if [[ $status = "FAILED" ]]; then
+  exit 1
+else
+  express_url=https://$(cf app express-api | awk '{print $2}' | sed -n 5p)/
+  echo express_url=$express_url
+fi
+
 app=nginx-angular
 
 cf push $app \
@@ -27,6 +35,7 @@ cf push $app \
 cf se $app MAPBOX_TOKEN $MAPBOX_TOKEN
 cf se $app KEYCLOAK_URL $keycloak_url
 cf se $app SPRINGBOOT_URL $springboot_url
+cf se $app EXPRESS_URL $express_url
 cf se $app KEYCLOAK false
 
 cf start $app
