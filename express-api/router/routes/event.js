f11daeeb29954ce4a68c23d6ef576280bc64b0e9
Mike Echevarria <mike.echevarria@gmail.com>
2020-06-05 10:02:38 -0400

    add additional columns to event output

--- express-api/router/routes/event.js
+++ express-api/router/routes/event.js
@@ -4,18 +4,29 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   const eventQuery = `
-	SELECT EVENT_ID as "eventId",
-       EVENT_DATE as "eventDate",
-       EVENT_TYPE as "eventType",
-       ACTOR_1 as "actor1",
-       ACTOR_2 as "actor2",
-       COUNTRY_NAME as "countryName",
-       FATALITIES as "fatalities",
-       GEO_LOCATION.ST_AsGeoJSON() as "geoLocation",
-       NOTES as "notes"
-	FROM EVENT
-	LIMIT ?
-	OFFSET ?
+	SELECT 
+	  EVENT_ID AS "eventId",
+	  EVENT_DATE AS "eventDate",
+	  EVENT_TYPE AS "eventType",
+	  SUB_EVENT_TYPE AS "subEventType",
+	  ACTOR_1 AS "actor1",
+	  ACTOR_2 AS "actor2",
+	  ASSOC_ACTOR_1 AS assocActor1,
+	  ASSOC_ACTOR_2 AS assocActor2,
+	  COUNTRY_NAME AS "country",
+	  LATITUDE AS "latitude",
+	  LONGITUDE AS "longitude",
+	  GEO_LOCATION.ST_AsGeoJSON() AS "geoLocation",
+	  LOCATION AS "location",
+	  NOTES AS "notes",
+	  REGION AS "region",
+	  SOURCE AS "source",
+	  FATALITIES AS "fatalities",
+	  TIMESTAMP AS "timestamp",
+	  YEAR AS "year"
+  FROM EVENT	
+  LIMIT ?
+  OFFSET ?
   `;
   const limit = req.query.limit || 10;
   const offset = req.query.offset || 0;
